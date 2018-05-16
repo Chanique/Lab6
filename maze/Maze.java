c89f5d7b18ade9b33782c320d968fc0e900fb017
Chanique Liljeroth Bäck <35966724+Chanique@users.noreply.github.com>
2018-05-16 10:59:33 +0200

    Update Maze.java

--- maze/Maze.java
+++ maze/Maze.java
@@ -7,7 +7,26 @@ public class Maze extends Board {
     }
     
     public void create() {
-//    	 Implement this method!
+      graph = new ExtendedGraph();
+      set = new DisjointSets(maxCell);
+      amountOfSets = maxCell;
+      
+      while(amountOfSets > 1){
+        int i; 
+        i = rand.nextInt(4);
+        
+        if(i == 0){
+          d = Point.Direction.UP;
+        }
+        if(i == 1){
+          d = Point.Direction.LEFT;
+        }
+        if(i == 2){
+          d = Point.Direction.RIGHT;
+        } else {
+          d = Point.Direction.DOWN;
+        }
+      }
     }
     
     public void search() {
